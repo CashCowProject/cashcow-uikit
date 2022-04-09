e527deb78baf3ea7f61e06ac837bc2a47afd5bc3
Matias Caparros <55497544+caparrosmatias@users.noreply.github.com>
2022-04-09 11:18:14 -0300

    fix: Change Hover Color

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2820,8 +2820,8 @@ var templateObject_1$K;
 var baseColors = {
     failure: "#ED4B9A",
     primary: "#7645D9",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primaryBright: "#7645D9",
+    primaryDark: "#7645D9",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
